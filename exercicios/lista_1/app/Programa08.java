fa03f2514d24d8a9a4787809e91b472212d9c8b8
andreluas <andreluas.k@gmail.com>
2021-06-20 20:02:44 -0300

    ex8 done

---
+++ exercicios/lista_1/app/Programa08.java
@@ -0,0 +1,27 @@
+package app;
+
+/**
+ * Crie um programa que apresente o fatorial de um número N informado
+pelo usuário.
+ */
+
+import java.util.Scanner;
+
+public class Programa08 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Insira um número: ");
+        int numero = sc.nextInt();
+
+        sc.close();
+
+        int fatorial = 1;
+        while(numero > 1){
+            fatorial = fatorial * numero;
+            numero = numero - 1;
+        }
+
+        System.out.println("O fatorial desse número é " + fatorial);
+    }
+}
